7a96b1693b2fb9bb38534ad5851b62572087fb3b
Alina <kalinka0611@mail.ru>
2023-01-08 18:17:26 +0300

    Решение 1 задачи

---
+++ Seminar_1/Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿int a = -3;
+int b = -9;
+
+if (a < b)
+{
+    Console.Write("max = ");
+    Console.WriteLine(b);
+    Console.Write("min = ");
+    Console.Write(a);
+}
+else
+{
+    Console.Write("max = ");
+    Console.WriteLine(a);
+    Console.Write("min = ");
+    Console.Write(b);
+}
