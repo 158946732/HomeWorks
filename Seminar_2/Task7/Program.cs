d43bcf7f14cf0196a476eb249097fc4c5e2909c1
Alina <kalinka0611@mail.ru>
2023-01-10 00:08:25 +0300

    Решение 3 задачи 2 семинара

--- Seminar_2/Task7/Program.cs
+++ Seminar_2/Task7/Program.cs
@@ -1,5 +1,13 @@
 ﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 // 6 -> да / 7 -> да / 1 -> нет
 
-Console.Write("Введите число от 1 до 7");
-string DayOfWeek = Console.ReadLine();
+Console.Write("Введите номер дня недели ");
+int DayOfWeek = Convert.ToInt32(Console.ReadLine());
+if (DayOfWeek % 7.1 > 5)
+{
+    System.Console.WriteLine("выходной");
+}
+else
+{
+    System.Console.WriteLine("будничный");
+}
