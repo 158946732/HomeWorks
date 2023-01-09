6e31eef12fcf81fc5283d5fedb49f96f067d7628
Alina <kalinka0611@mail.ru>
2023-01-09 23:03:55 +0300

    Скорректировано решение  задачи Task6

--- Seminar_2/Task6/Program.cs
+++ Seminar_2/Task6/Program.cs
@@ -1,14 +1,16 @@
 ﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 // 645 -> 5 / 78 -> третьей цифры нет / 32679 -> 6
 
-int N = 32679;
-string number3 = N.ToString();
-
-if (number3.Length > 2)
+int number = 32679;
+if (number < 100)
 {
-    Console.WriteLine(number3[2]);
+    Console.WriteLine("Третьей цифры нет");
 }
 else
 {
-    Console.WriteLine("третьей цифры нет");
+    while (number > 1000)
+    {
+        number = number / 10;
+    }
+    System.Console.WriteLine(number % 10);
 }
