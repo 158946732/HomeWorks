13f61968d6cdd2e2daf46233264f000eda3b16e2
Alina <kalinka0611@mail.ru>
2023-01-17 01:27:07 +0300

    Решение 1(41) задачи 6 семинара

---
+++ Seminar6/Task14(41)/Program.cs
@@ -0,0 +1,28 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2 // 1, -7, 567, 89, 223-> 4
+
+int GetCountByUser()
+{
+    Console.Write("Введите количество чисел: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int m = GetCountByUser();
+Console.WriteLine(m);
+
+Console.WriteLine("Введите любые числа: ");
+
+int count = 0;
+
+for (int i = 0; i < m; i++)
+{
+    int numbers = Convert.ToInt32(Console.ReadLine());
+    if (numbers > 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"Чисел больше нуля: {count}");
+
+
