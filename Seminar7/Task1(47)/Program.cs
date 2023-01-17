0748f7dd393d8d13e10c21e92cb2bf7389caa84e
Alina <kalinka0611@mail.ru>
2023-01-18 00:19:18 +0300

    Решение задачи 1(47) семинара 7

---
+++ Seminar7/Task1(47)/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+double[,] CreateArray()
+{
+    Console.WriteLine("Введите количество строк  ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите количество столбцов ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    double[,] matrix = new double[rows,columns];
+    
+    return matrix;
+}
+
+void FillMatrix(double[,] matr)
+{
+
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) /10;
+            Console.Write($"{matr[i, j],6}");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] matrix = CreateArray();
+FillMatrix(matrix);
