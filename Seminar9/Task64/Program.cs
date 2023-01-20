f1d7e2c24d72ddd03f77babeecfa8fb03f888f86
Alina <kalinka0611@mail.ru>
2023-01-21 00:02:46 +0300

    Решение задачи 1 семинара 9

---
+++ Seminar9/Task64/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int GetNumber()
+{
+    Console.WriteLine("Введите число N (больше единицы) ");
+    return int.Parse(Console.ReadLine());
+}
+
+int endNumber = GetNumber();
+
+void ShowInt(int number)
+{
+    if (number < 0) Console.Write($"{number} не натуральное число");
+    if (number == 0) return;
+    Console.Write("{0,4}", number);
+    ShowInt(number - 1);
+}
+ShowInt(endNumber);
